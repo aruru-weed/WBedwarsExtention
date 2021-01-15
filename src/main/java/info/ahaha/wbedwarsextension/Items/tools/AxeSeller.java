6c51a86052a08af950e2c4a8c4f30953216fd9e8
kaogon128 <bump_love128@yahoo.co.jp>
2021-01-15 18:09:32 +0900

    Revert "Merge branch 'main' of https://github.com/kaogon128/WBedwarsExtention into main"
    This reverts commit c6f632e8d521d0ea8b7841452132c8505f6c8115, reversing
    changes made to 556cb74f77ffb39d163c80022d35af8f4bbe26d4.

--- src/main/java/info/ahaha/wbedwarsextension/Items/tools/AxeSeller.java
+++ src/main/java/info/ahaha/wbedwarsextension/Items/tools/AxeSeller.java
@@ -3,11 +3,9 @@ package info.ahaha.wbedwarsextension.Items.tools;
 import info.ahaha.bedwars.API.Game;
 import info.ahaha.guiapi.abstract_GUI_Item;
 import info.ahaha.guiapi.v2.Icon.Icon;
-import info.ahaha.guiapi.v2.Icon.IconFactory;
 import info.ahaha.guiapi.v2.extended.items.AbstractPerPlayerItem;
 import info.ahaha.wbedwarsextension.Items.AbstractSeller;
 import info.ahaha.wbedwarsextension.WBedwarsExtension;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -15,7 +13,6 @@ public class AxeSeller extends AbstractPerPlayerItem {
     public AxeSeller(Game game, int slot) {
         super(slot);
         this.game = game;
-
     }
 
     Game game;
@@ -41,13 +38,5 @@ public class AxeSeller extends AbstractPerPlayerItem {
     @Override
     public abstract_GUI_Item getDefault() {
         return null;
-    }
-
-    public static ItemStack get(int num) {
-        switch (num) {
-            break;
-            case 1:
-                return IconFactory.Make(Material.WOOD_AXE).addEnchant(new Icon.EnchantMeta(E))
-        }
     }
 }
