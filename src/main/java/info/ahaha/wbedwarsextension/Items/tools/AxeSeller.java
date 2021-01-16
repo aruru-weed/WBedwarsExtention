f5597b2d01cc2d822199ca05ed83536569c69bdf
aruru-weed <aruzenyukkuri@gmail.com>
2021-01-16 19:31:10 +0900

    add Tools get()

--- src/main/java/info/ahaha/wbedwarsextension/Items/tools/AxeSeller.java
+++ src/main/java/info/ahaha/wbedwarsextension/Items/tools/AxeSeller.java
@@ -3,9 +3,12 @@ package info.ahaha.wbedwarsextension.Items.tools;
 import info.ahaha.bedwars.API.Game;
 import info.ahaha.guiapi.abstract_GUI_Item;
 import info.ahaha.guiapi.v2.Icon.Icon;
+import info.ahaha.guiapi.v2.Icon.IconFactory;
 import info.ahaha.guiapi.v2.extended.items.AbstractPerPlayerItem;
 import info.ahaha.wbedwarsextension.Items.AbstractSeller;
 import info.ahaha.wbedwarsextension.WBedwarsExtension;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -42,6 +45,16 @@ public class AxeSeller extends AbstractPerPlayerItem {
     }
 
     public static ItemStack get(int num) {
+        switch (num) {
+            case 1:
+                return IconFactory.Make(Material.WOOD_AXE).addEnchant(new Icon.EnchantMeta(Enchantment.DIG_SPEED, 2)).toItem();
+            case 2:
+                return IconFactory.Make(Material.STONE_AXE).addEnchant(new Icon.EnchantMeta(Enchantment.DIG_SPEED, 1)).toItem();
+            case 3:
+                return IconFactory.Make(Material.IRON_AXE).addEnchant(new Icon.EnchantMeta(Enchantment.DIG_SPEED, 1)).toItem();
+            case 4:
+                return IconFactory.Make(Material.GOLD_AXE).addEnchant(new Icon.EnchantMeta(Enchantment.DIG_SPEED, 3)).toItem();
+        }
         return null;
     }
 }
